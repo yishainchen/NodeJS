92b10dae9badf6f37ddde48e46239b3a02a8e3fd
B1media <chenyishain@gmail.com>
2016-06-29 18:03:33 +0800

    mongeDB connect bug fix

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/testproject');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var about = require('./routes/about');
@@ -24,8 +28,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Allow router to access db
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 app.use('/', routes);
-app.get('/users', users);
+app.use('/users', users);
 app.get('/about', about);
 // app.get('/userlist', userlist);
 
