2d30885b7755a2e6693bbc06e16f507131a7c52d
B1media <chenyishain@gmail.com>
2016-06-29 16:49:07 +0800

    add user Page

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var about = require('./routes/about');
+// var userlist = require('./routes/userlist');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hjs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,8 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.get('/users', users);
 app.get('/about', about);
+// app.get('/userlist', userlist);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+ 
+      var name = [];
+      var objKey = Object.keys(docs);
+      objKey.forEach(function(objectid){
+        var itemkeys = Object.keys(docs[objectid]);
+        itemkeys.forEach(function(itemkey) {
+          var itemvalue =docs[objectid][itemkey];
+          console.log(objectid+': '+itemkey+' = '+itemvalue);
+          if (itemkey == "username") {
+            name.push(itemvalue);
+          }
+        })
+      })
+      console.log(name);
+      res.render('userlist', {
+          "userlist" : name
+      });
+    });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET New User page. */
+router.get('/adduser', function(req, res) {
+    res.render('adduser', { title: 'Add New User' });
+});
+
+
 module.exports = router;
+
+
