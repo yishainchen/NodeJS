6caed342f3166406bbdfd22c58565c5c7e694240
B1media <chenyishain@gmail.com>
2016-06-29 16:57:24 +0800

    add user info to DB

--- routes/index.js
+++ routes/index.js
@@ -37,6 +37,37 @@ router.get('/adduser', function(req, res) {
 });
 
 
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+ 
+    // Set our internal DB variable
+    var db = req.db;
+ 
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var Email = req.body.email;
+ 
+    // Set our collection
+    var collection = db.get('usercollection');
+ 
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+        "email" : Email
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            res.location("userlist");
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
+
 module.exports = router;
 
 
